49e338fe7695f439cf3cb4061f534bccc3480c9d
Debdyuti Das <48562834+Debdyuti-01@users.noreply.github.com>
2024-03-07 20:58:24 -0800

    Add files via upload

---
+++ src/Components/Home/Home2.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import girl from "../../Assets/laptop.jpeg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              Let me <span className="purple"> introduce</span> myself ..
+            </h1>
+            <p className="home-about-body">
+            I developed a keen interest in programming and acquired foundational knowledge, or so I believe... 🤷‍♂️
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple"> Java, Python, C++, Javascript and Go </b>
+              </i>
+              <br />
+              <br />
+              My field of Interests are building new &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products </b> 
+                <b className="purple">
+                  
+                </b>
+              </i>
+              <br />
+              <br />
+              Whenever possible, I apply my passion for developing products
+              with <b className="purple">Node.js</b> and
+              <i>
+                <b className="purple">
+                  {" "}
+                  Modern Javascript Library and Frameworks
+                </b>
+              </i>
+              &nbsp; like
+              <i>
+                <b className="purple"> React.js and Next.js</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={girl} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>Find me on</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Debdyuti-01"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/debdyuti-das/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+             
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
